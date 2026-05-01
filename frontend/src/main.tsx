4fc1e7ed6f034c65da62b610e579bd367fc7ec72
uptokendev <uptokendev@gmail.com>
2026-05-01 13:08:44 +0200

    Load MemeWarzone HUD styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,5 +6,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./styles/mwz-hud.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
