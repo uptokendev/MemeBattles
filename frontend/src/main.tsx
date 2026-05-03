215a89479e97a4dd8d040b672aa20e4040b0967b
uptokendev <uptokendev@gmail.com>
2026-05-03 23:03:39 +0200

    Load tactical CSS after legacy HUD styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,5 +7,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./styles/mwz-hud.css";
+import "./styles/tactical-command-ui.css";
+import "./styles/tactical-hero.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
