03447c0c4d8f9c26cede875286e2c74ee4140338
uptokendev <uptokendev@gmail.com>
2026-05-05 13:03:42 +0200

    Install profile draft following count patch

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,5 +9,6 @@ import "./index.css";
 import "./styles/mwz-hud.css";
 import "./styles/tactical-command-ui.css";
 import "./lib/prepareCloseoutBridge";
+import "./lib/profileDraftFollowingCountPatch";
 
 createRoot(document.getElementById("root")!).render(<App />);
