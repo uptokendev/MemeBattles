29893c2b0b5f9e9a6895ff159ab46018fa4726e5
uptokendev <uptokendev@gmail.com>
2026-05-05 13:22:12 +0200

    Disable unsafe profile count DOM patch

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,5 @@ import "./index.css";
 import "./styles/mwz-hud.css";
 import "./styles/tactical-command-ui.css";
 import "./lib/prepareCloseoutBridge";
-import "./lib/profileDraftFollowingCountPatch";
 
 createRoot(document.getElementById("root")!).render(<App />);
