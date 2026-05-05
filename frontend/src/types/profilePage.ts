0ef29b204a3bf4565ebc8d99b8082fc2a0fad015
uptokendev <uptokendev@gmail.com>
2026-05-05 13:56:16 +0200

    refactor(profile): add profile page balance type

---
+++ frontend/src/types/profilePage.ts
@@ -0,0 +1,9 @@
+export type TokenBalanceRow = {
+  campaignAddress: string;
+  tokenAddress: string;
+  image: string;
+  name: string;
+  ticker: string;
+  balanceRaw: bigint;
+  balanceFormatted: string;
+};
