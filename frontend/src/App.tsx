700ea01915e494616e511b1dfc310e308cfc2e1f
uptokendev <uptokendev@gmail.com>
2026-05-17 04:12:40 +0200

    Inserted warzone hud in promotion page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -136,7 +136,7 @@ const App = () => {
                 <TopBar mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
                 <RankPromotionListener />
                 <LiveStreamOverlay />
-                <main className="flex-1 overflow-auto scroll-pt-[4rem] md:scroll-pt-[4.25rem] pt-[4.5rem] md:pt-[4.75rem] px-2 md:px-3 lg:px-4 pb-4 md:pb-6 lg:pb-8">
+                <main className="flex-1 overflow-auto scroll-pt-[3.25rem] md:scroll-pt-[4.25rem] pt-[2rem] md:pt-[3rem] px-2 md:px-3 lg:px-4 pb-4 md:pb-6 lg:pb-8">
                   <Routes>
                     <Route path="/" element={<Showcase />} />
                     <Route path="/create" element={<Create />} />
