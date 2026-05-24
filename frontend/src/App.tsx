e1d1a21ff25282ab07cd88a3cd2304d8a8c1f127
uptokendev <uptokendev@gmail.com>
2026-05-24 22:41:26 +0200

    Wire command center arena ops route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -60,6 +60,7 @@ import CommandCenterClaims from "@/pages/command-center/CommandCenterClaims";
 import CommandCenterSettings from "@/pages/command-center/CommandCenterSettings";
 import CommandCenterSocial from "@/pages/command-center/CommandCenterSocial";
 import CommandCenterCoins from "@/pages/command-center/CommandCenterCoins";
+import CommandCenterArenaOps from "@/pages/command-center/CommandCenterArenaOps";
 import { isPostGradRouteEnabled, postGradFlags } from "@/features/postgrad/config";
 
 const queryClient = new QueryClient();
@@ -160,6 +161,7 @@ function AppShellLayout({
           <Route path="/command/followers" element={<LegacyCommandCenterRedirect section="followers" />} />
           <Route path="/command/following" element={<LegacyCommandCenterRedirect section="following" />} />
           <Route path="/command/coins" element={<LegacyCommandCenterRedirect section="coins" />} />
+          <Route path="/command/arena-ops" element={<LegacyCommandCenterRedirect section="arena-ops" />} />
           <Route path="/command/*" element={<LegacyCommandCenterRedirect section="overview" />} />
           <Route path="/profile/:wallet/command" element={<CommandCenterShell><CommandCenterOverview /></CommandCenterShell>} />
           <Route path="/profile/:wallet/command/overview" element={<CommandCenterShell><CommandCenterOverview /></CommandCenterShell>} />
@@ -171,6 +173,7 @@ function AppShellLayout({
           <Route path="/profile/:wallet/command/followers" element={<CommandCenterShell><CommandCenterSocial mode="followers" /></CommandCenterShell>} />
           <Route path="/profile/:wallet/command/following" element={<CommandCenterShell><CommandCenterSocial mode="following" /></CommandCenterShell>} />
           <Route path="/profile/:wallet/command/coins" element={<CommandCenterShell><CommandCenterCoins /></CommandCenterShell>} />
+          <Route path="/profile/:wallet/command/arena-ops" element={<CommandCenterShell><CommandCenterArenaOps /></CommandCenterShell>} />
           <Route path="/profile/:wallet/command/*" element={<CommandCenterShell><CommandCenterOverview /></CommandCenterShell>} />
           <Route path="/profile/:identifier" element={<ProfilePage />} />
           <Route path="/profile/:wallet/*" element={<ProfileWalletFallbackRedirect />} />
