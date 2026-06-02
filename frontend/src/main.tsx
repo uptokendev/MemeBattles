f9c7eb33792b5ffb19f8bf6a87103f6d38951273
uptokendev <uptokendev@gmail.com>
2026-06-02 22:28:06 +0200

    Import landing styles on dev

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import App from "./App.tsx";
 import "./index.css";
 import "./styles/mwz-hud.css";
 import "./styles/tactical-command-ui.css";
+import "./styles/landing-page.css";
 import "./styles/page-density-fixes.css";
 import "./styles/card-cleanup.css";
 import "./styles/prepare-title-fix.css";
