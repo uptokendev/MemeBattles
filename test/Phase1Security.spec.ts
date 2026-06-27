50102675ac3cf4bc3e8297ddefafb5f1b02ac83a
uptokendev <uptokendev@gmail.com>
2026-06-27 12:32:03 +0200

    Fix creator buy cap security test curve pricing

--- test/Phase1Security.spec.ts
+++ test/Phase1Security.spec.ts
@@ -73,13 +73,17 @@ describe("Phase 1 security layer", function () {
   it("enforces creator buy cap after the lock expires", async () => {
     const { factory, creator } = await deployCoreFixture();
     await deployRegistries(factory);
-    const { campaign } = await createCampaign(factory, creator);
+    const { campaign } = await createCampaign(factory, creator, {
+      basePrice: ethers.parseEther("0.001"),
+      priceSlope: 1n,
+      graduationTarget: ethers.parseEther("100"),
+    });
 
     await ethers.provider.send("evm_increaseTime", [24 * 60 * 60 + 1]);
     await ethers.provider.send("evm_mine", []);
 
     await expect(
-      campaign.connect(creator).buyExactBnb(0n, { value: ethers.parseEther("1") })
+      campaign.connect(creator).buyExactBnb(0n, { value: ethers.parseEther("0.3") })
     ).to.be.revertedWith("creator buy cap");
   });
 
