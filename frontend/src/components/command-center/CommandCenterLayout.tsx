b6ea3011bb7a34e070efb503d4c273fee2199a21
uptokendev <uptokendev@gmail.com>
2026-07-05 01:25:27 +0200

    Drop command center below topbar

--- frontend/src/components/command-center/CommandCenterLayout.tsx
+++ frontend/src/components/command-center/CommandCenterLayout.tsx
@@ -14,7 +14,7 @@ type CommandCenterLayoutProps = {
 export function CommandCenterLayout({ walletAddress, basePath, children }: CommandCenterLayoutProps) {
   return (
     <CommandCenterDataProvider walletAddress={walletAddress}>
-      <ContentContainer className="space-y-4 pb-8">
+      <ContentContainer className="mwz-command-center-layout space-y-4 pb-8 pt-20 md:pt-24 lg:pt-28">
         <CommandCenterHero walletAddress={walletAddress} />
         <div className="grid gap-4 lg:grid-cols-[280px_minmax(0,1fr)]">
           <CommandCenterSidebar basePath={basePath} />
