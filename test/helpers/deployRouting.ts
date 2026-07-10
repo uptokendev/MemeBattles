82f3f2c85e48a5e05bfef7abc5ba9eb3815d6a7d
uptokendev <uptokendev@gmail.com>
2026-07-10 16:22:27 +0200

    Add shared routed launch test deployment helper

---
+++ test/helpers/deployRouting.ts
@@ -0,0 +1,51 @@
+import { ethers } from "hardhat";
+import { deployLaunchFactory } from "./deployFactory";
+
+export async function deployMockDexRouter(wrappedAddress: string) {
+  const V2Factory = await ethers.getContractFactory("MockV2Factory");
+  const v2factory = await V2Factory.deploy();
+  await v2factory.waitForDeployment();
+
+  const DexRouter = await ethers.getContractFactory("MockRouter");
+  const dexRouter = await DexRouter.deploy(await v2factory.getAddress(), wrappedAddress);
+  await dexRouter.waitForDeployment();
+
+  return { v2factory, dexRouter };
+}
+
+export async function deployConfiguredTreasuryRouter(adminAddress: string) {
+  const AcceptingReceiver = await ethers.getContractFactory("AcceptingReceiver");
+  const leagueVault = await AcceptingReceiver.deploy();
+  const recruiterVault = await AcceptingReceiver.deploy();
+  const protocolVault = await AcceptingReceiver.deploy();
+  await Promise.all([
+    leagueVault.waitForDeployment(),
+    recruiterVault.waitForDeployment(),
+    protocolVault.waitForDeployment(),
+  ]);
+
+  const TreasuryRouter = await ethers.getContractFactory("TreasuryRouter");
+  const treasuryRouter = await TreasuryRouter.deploy(adminAddress, await leagueVault.getAddress(), 3600);
+  await treasuryRouter.waitForDeployment();
+
+  const CommunityRewardsVault = await ethers.getContractFactory("CommunityRewardsVault");
+  const communityVault = await CommunityRewardsVault.deploy(adminAddress, await treasuryRouter.getAddress());
+  await communityVault.waitForDeployment();
+
+  await treasuryRouter.setRecruiterRewardsVault(await recruiterVault.getAddress());
+  await treasuryRouter.setCommunityRewardsVault(await communityVault.getAddress());
+  await treasuryRouter.setProtocolRevenueVault(await protocolVault.getAddress());
+
+  return { treasuryRouter, leagueVault, recruiterVault, protocolVault, communityVault };
+}
+
+export async function deployRoutedLaunchFactory(admin: any) {
+  const { dexRouter, v2factory } = await deployMockDexRouter(await admin.getAddress());
+  const routing = await deployConfiguredTreasuryRouter(await admin.getAddress());
+  const { factory, campaignImplementation } = await deployLaunchFactory(
+    await dexRouter.getAddress(),
+    await routing.treasuryRouter.getAddress()
+  );
+
+  return { dexRouter, v2factory, ...routing, factory, campaignImplementation };
+}
