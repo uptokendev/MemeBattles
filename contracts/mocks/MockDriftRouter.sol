80c1f7a98088f3e26349f2f60887c1e11c572566
uptokendev <uptokendev@gmail.com>
2026-07-11 02:29:23 +0200

    test: add drift router mock for graduation price checks

---
+++ contracts/mocks/MockDriftRouter.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {IPancakeRouter02} from "../interfaces/IPancakeRouter02.sol";
+import {MockV2Factory} from "./MockV2Factory.sol";
+import {MockV2Pair} from "./MockV2Pair.sol";
+
+contract MockDriftRouter is IPancakeRouter02 {
+    address private immutable _factory;
+    address private immutable _wrapped;
+
+    constructor(address factory_, address wrapped_) {
+        _factory = factory_;
+        _wrapped = wrapped_;
+    }
+
+    function factory() external view override returns (address) {
+        return _factory;
+    }
+
+    function WETH() external view override returns (address) {
+        return _wrapped;
+    }
+
+    function addLiquidityETH(
+        address token,
+        uint256 amountTokenDesired,
+        uint256,
+        uint256,
+        address,
+        uint256
+    )
+        external
+        payable
+        override
+        returns (uint256 amountToken, uint256 amountETH, uint256 liquidity)
+    {
+        amountToken = amountTokenDesired / 2;
+        amountETH = msg.value;
+        liquidity = amountToken + amountETH;
+
+        IERC20(token).transferFrom(msg.sender, address(this), amountToken);
+
+        address pair = MockV2Factory(_factory).getPair(token, _wrapped);
+        if (pair == address(0)) pair = MockV2Factory(_factory).createPair(token, _wrapped);
+        MockV2Pair(pair).setReserves(uint112(amountToken), uint112(amountETH));
+        MockV2Pair(pair).setTotalSupply(1);
+    }
+}
