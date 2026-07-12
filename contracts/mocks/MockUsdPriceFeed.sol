656522d9d660f59cced4c77b31478b11b1ae26b3
uptokendev <uptokendev@gmail.com>
2026-07-12 12:28:30 +0200

    Add mock USD price feed

---
+++ contracts/mocks/MockUsdPriceFeed.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract MockUsdPriceFeed {
+    uint8 public immutable decimals;
+    uint80 public roundId;
+    int256 public answer;
+    uint256 public startedAt;
+    uint256 public updatedAt;
+    uint80 public answeredInRound;
+
+    constructor(uint8 decimals_) {
+        decimals = decimals_;
+    }
+
+    function setRoundData(
+        uint80 roundId_,
+        int256 answer_,
+        uint256 startedAt_,
+        uint256 updatedAt_,
+        uint80 answeredInRound_
+    ) external {
+        roundId = roundId_;
+        answer = answer_;
+        startedAt = startedAt_;
+        updatedAt = updatedAt_;
+        answeredInRound = answeredInRound_;
+    }
+
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId_, int256 answer_, uint256 startedAt_, uint256 updatedAt_, uint80 answeredInRound_)
+    {
+        return (roundId, answer, startedAt, updatedAt, answeredInRound);
+    }
+}
