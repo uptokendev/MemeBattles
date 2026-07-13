b5c5e29d2c2d9c9186ef5e1b2f0be01caffc255e
uptokendev <uptokendev@gmail.com>
2026-07-13 11:33:33 +0200

    test: make drift router mock Topaz-compatible

--- contracts/mocks/MockDriftRouter.sol
+++ contracts/mocks/MockDriftRouter.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.24;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {IPancakeRouter02} from "../interfaces/IPancakeRouter02.sol";
+import {ITopazRouter02} from "../interfaces/ITopazRouter02.sol";
 import {MockV2Factory} from "./MockV2Factory.sol";
 import {MockV2Pair} from "./MockV2Pair.sol";
 
-contract MockDriftRouter is IPancakeRouter02 {
+contract MockDriftRouter is IPancakeRouter02, ITopazRouter02 {
     address private immutable _factory;
     address private immutable _wrapped;
 
@@ -20,7 +21,11 @@ contract MockDriftRouter is IPancakeRouter02 {
         return _factory;
     }
 
-    function WETH() external view override returns (address) {
+    function poolFactory() external view override returns (address) {
+        return _factory;
+    }
+
+    function WETH() external view override(IPancakeRouter02, ITopazRouter02) returns (address) {
         return _wrapped;
     }
 
@@ -34,7 +39,32 @@ contract MockDriftRouter is IPancakeRouter02 {
     )
         external
         payable
-        override
+        override(IPancakeRouter02)
+        returns (uint256 amountToken, uint256 amountETH, uint256 liquidity)
+    {
+        return _addDriftLiquidity(token, amountTokenDesired);
+    }
+
+    function addLiquidityETH(
+        address token,
+        bool stable,
+        uint256 amountTokenDesired,
+        uint256,
+        uint256,
+        address,
+        uint256
+    )
+        external
+        payable
+        override(ITopazRouter02)
+        returns (uint256 amountToken, uint256 amountETH, uint256 liquidity)
+    {
+        require(!stable, "stable pool unsupported");
+        return _addDriftLiquidity(token, amountTokenDesired);
+    }
+
+    function _addDriftLiquidity(address token, uint256 amountTokenDesired)
+        internal
         returns (uint256 amountToken, uint256 amountETH, uint256 liquidity)
     {
         amountToken = amountTokenDesired / 2;
