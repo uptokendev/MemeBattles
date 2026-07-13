39141ad1cc4cca7ac1e69a5c958e7bb159388d7c
uptokendev <uptokendev@gmail.com>
2026-07-13 11:35:37 +0200

    test: add pair aliases to Topaz factory mock

--- contracts/mocks/MockTopazFactory.sol
+++ contracts/mocks/MockTopazFactory.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.24;
 
 import {MockTopazPool} from "./MockTopazPool.sol";
 
+interface IPairTokenSetter {
+    function setTokens(address token0, address token1) external;
+}
+
 /// @dev Minimal Topaz v2 factory mock with stable/volatile pool separation.
 contract MockTopazFactory {
     mapping(bytes32 => address) public pools;
@@ -16,6 +20,11 @@ contract MockTopazFactory {
         MockTopazPool(pool).setTokens(tokenA < tokenB ? tokenA : tokenB, tokenA < tokenB ? tokenB : tokenA, stable);
     }
 
+    function setPair(address tokenA, address tokenB, address pair) external {
+        pools[_key(tokenA, tokenB, false)] = pair;
+        IPairTokenSetter(pair).setTokens(tokenA < tokenB ? tokenA : tokenB, tokenA < tokenB ? tokenB : tokenA);
+    }
+
     function createPool(address tokenA, address tokenB, bool stable) external returns (address pool) {
         bytes32 key = _key(tokenA, tokenB, stable);
         pool = pools[key];
@@ -26,7 +35,15 @@ contract MockTopazFactory {
         pools[key] = pool;
     }
 
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        return this.createPool(tokenA, tokenB, false);
+    }
+
     function getPool(address tokenA, address tokenB, bool stable) external view returns (address pool) {
         return pools[_key(tokenA, tokenB, stable)];
+    }
+
+    function getPair(address tokenA, address tokenB) external view returns (address pair) {
+        return pools[_key(tokenA, tokenB, false)];
     }
 }
