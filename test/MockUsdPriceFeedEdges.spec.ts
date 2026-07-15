83f3b2c6644a3a0e4075488b81654d70cea43ba4
uptokendev <uptokendev@gmail.com>
2026-07-15 18:17:59 +0200

    test: read mock price feed tuple positions

--- test/MockUsdPriceFeedEdges.spec.ts
+++ test/MockUsdPriceFeedEdges.spec.ts
@@ -19,11 +19,11 @@ describe("MockUsdPriceFeed", function () {
     await feed.waitForDeployment();
 
     const data = await feed.latestRoundData();
-    expect(data.roundId).to.eq(0n);
-    expect(data.answer).to.eq(0n);
-    expect(data.startedAt).to.eq(0n);
-    expect(data.updatedAt).to.eq(0n);
-    expect(data.answeredInRound).to.eq(0n);
+    expect(data[0]).to.eq(0n);
+    expect(data[1]).to.eq(0n);
+    expect(data[2]).to.eq(0n);
+    expect(data[3]).to.eq(0n);
+    expect(data[4]).to.eq(0n);
   });
 
   it("stores and overwrites full round data", async () => {
@@ -33,18 +33,18 @@ describe("MockUsdPriceFeed", function () {
 
     await feed.setRoundData(1n, ethers.parseUnits("600", 8), 10n, 11n, 1n);
     let data = await feed.latestRoundData();
-    expect(data.roundId).to.eq(1n);
-    expect(data.answer).to.eq(ethers.parseUnits("600", 8));
-    expect(data.startedAt).to.eq(10n);
-    expect(data.updatedAt).to.eq(11n);
-    expect(data.answeredInRound).to.eq(1n);
+    expect(data[0]).to.eq(1n);
+    expect(data[1]).to.eq(ethers.parseUnits("600", 8));
+    expect(data[2]).to.eq(10n);
+    expect(data[3]).to.eq(11n);
+    expect(data[4]).to.eq(1n);
 
     await feed.setRoundData(2n, -1n, 20n, 21n, 1n);
     data = await feed.latestRoundData();
-    expect(data.roundId).to.eq(2n);
-    expect(data.answer).to.eq(-1n);
-    expect(data.startedAt).to.eq(20n);
-    expect(data.updatedAt).to.eq(21n);
-    expect(data.answeredInRound).to.eq(1n);
+    expect(data[0]).to.eq(2n);
+    expect(data[1]).to.eq(-1n);
+    expect(data[2]).to.eq(20n);
+    expect(data[3]).to.eq(21n);
+    expect(data[4]).to.eq(1n);
   });
 });
