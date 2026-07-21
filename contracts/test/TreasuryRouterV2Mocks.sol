5850f60e2c971954a5cd47a4c29ee2d20a875ff5
uptokendev <uptokendev@gmail.com>
2026-07-21 15:16:58 +0200

    Add TreasuryRouterV2 test mocks

---
+++ contracts/test/TreasuryRouterV2Mocks.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TreasuryRouterReceiverMock {
+    uint256 public received;
+
+    receive() external payable {
+        received += msg.value;
+    }
+}
+
+contract CommunityRewardsVaultMock {
+    uint256 public airdropReceived;
+    uint256 public squadReceived;
+
+    function depositAirdrop() external payable {
+        airdropReceived += msg.value;
+    }
+
+    function depositSquadPool() external payable {
+        squadReceived += msg.value;
+    }
+}
+
+contract RevertingTreasuryReceiverMock {
+   
