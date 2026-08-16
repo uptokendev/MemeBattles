2315df3e5463936b1147d1a6909d8c1c1eecbaa7
uptokendev <uptokendev@gmail.com>
2026-08-16 16:23:54 +0200

    Start canonical candle realtime publisher

--- realtime-indexer/src/main.ts
+++ realtime-indexer/src/main.ts
@@ -1,3 +1,4 @@
+import { startCanonicalCandleRealtimeLoop } from "./canonicalCandleRealtime.js";
 import { startSupportedFactoryDiscoveryLoop } from "./factoryDiscovery.js";
 import { startMeteoraSwapIndexerLoop } from "./meteoraSwapIndexer.js";
 import { startSolanaIndexerLoop } from "./solanaIndexer.js";
@@ -5,4 +6,5 @@ import { startSolanaIndexerLoop } from "./solanaIndexer.js";
 startSupportedFactoryDiscoveryLoop();
 startSolanaIndexerLoop();
 startMeteoraSwapIndexerLoop();
+startCanonicalCandleRealtimeLoop();
 await import("./server.js");
